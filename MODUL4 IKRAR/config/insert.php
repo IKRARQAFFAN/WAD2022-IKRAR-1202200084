6702e8301815583d4a9a2730bc100402bcdd2489
IKRARQAFFAN <ikrar.qaffan03@gmail.com>
2022-11-27 21:29:17 +0700

    modul3

---
+++ MODUL4 IKRAR/config/insert.php
@@ -0,0 +1,24 @@
+<?php
+require './connector.php';
+
+$namamobil = $_POST['nama'];
+$namapemilik = $_POST['pemilik'];
+$merk = $_POST['merk'];
+$tanggalbeli = $_POST['tanggalbeli'];
+$deskripsi = $_POST['desc'];
+$status = $_POST['status'];
+
+$gambar = $_FILES['gambar']['name'];
+
+$target = "../asset/images/";
+
+if (move_uploaded_file($_FILES['gambar']['tmp_name'], $target . $gambar)) {
+  $sql = "INSERT INTO showroom_Ikrar_table (nama_mobil, pemilik_mobil, merk_mobil, tanggal_beli, deskripsi, foto_mobil, status_pembayaran) VALUES ('$namamobil', '$namapemilik', '$merk', '$tanggalbeli', '$deskripsi', '$gambar', '$status')";
+  if (mysqli_query($connector, $sql)) {
+    header("location: ../pages/List-Ikrar.php?pesan=berhasil");
+  } else {
+    header("location: ../pages/List-Ikrar.php?pesan=gagal");
+  }
+} else {
+  header("location: ../pages/List-Ikrar.php?pesan=gagal");
+}
---
+++ MODUL4 IKRAR/pages/Profile-Ikrar.php
@@ -0,0 +1,41 @@
+<?php
+require "../config/connector.php";
+session_start();
+
+$pilihan_warna = [
+    "danger" => "Red",
+    "warning" => "Yellow",
+    "success" => "Green",
+    "primary" => "Blue",
+
+];
+
+if (isset($_SESSION["login"])) {
+    $login_as = $_SESSION["email"];
+    $result_login = mysqli_query($connector, "SELECT * FROM user_Ikrar WHERE email = '$login_as'");
+    $data_login = mysqli_fetch_assoc($result_login);
+} else {
+    header("Location: Login-Ikrar.php");
+    exit;
+}
+
+if (isset($_POST["update"])) {
+    $email = $_POST["email"];
+    $nama = $_POST["nama"];
+    $no_hp = $_POST["no_hp"];
+    $password = mysqli_real_escape_string($connector, $_POST["password"]);
+    $konfirmasi_password = mysqli_real_escape_string($connector, $_POST["password2"]);
+    setcookie("warna_navbar", $_POST["warna_navbar"], time() + 86400, "/");
+
+    if ($password == $konfirmasi_password) {
+        $query = "UPDATE user_Ikrar SET
+                nama = '$nama',
+                no_hp = '$no_hp',
+                password = '$password'
+              WHERE email = '$email'";
+        mysqli_query($connector, $query);
+    }
+
+    echo "<meta http-equiv='refresh' content='0'>";
+}
+?>
